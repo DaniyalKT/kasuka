8effbeddac1b07349f1b54008da407784f003dba
دانیال کفائی نژاد طهرانی <65863212+DaniyalKT@users.noreply.github.com>
2023-12-08 20:34:17 +0330

    footer>footerbottom is done

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,6 +3,20 @@ import './Footer.css'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <div className="footer-up"></div>
+      <div className="footer-bottom">
+        <div className="copyright">
+          <strong>
+            <span> Kasuka </span>
+          </strong>       
+          همه حقوق محفوظ است
+        </div>
+        <div className="credits">
+          طراحی شده توسط 
+          <a href='#'> Danial K-Tehrani</a>
+        </div>
+      </div>
+    </div>
   )
 }
